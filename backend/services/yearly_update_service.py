d0fffacb516fe16b3f9aa524639a064ab27d25e7
willzoo <zhujwill@gmail.com>
2024-06-18 11:44:50 +0000

    Added error log to gitignore

--- backend/services/yearly_update_service.py
+++ backend/services/yearly_update_service.py
@@ -119,22 +119,28 @@ def schedule_yearly_task():
     move_grade_levels_for_all_schools(current_year, new_year)
 
 if __name__ == "__main__":
+    print("Type Update to trigger the update service")
+
+    command = input()
+
+    if command == "Update":
+        move_grade_levels_for_all_schools("2023-2024", "2024-2025")
+
+
     # print(current_app['MONGO_URI'])
     # scheduler = BackgroundScheduler()
     # # Schedule the task to run every year on August 7th at 00:00
     # scheduler.add_job(schedule_yearly_task, 'cron', month=8, day=7, hour=0, minute=0)
 
     # scheduler.start()
-    print("Scheduler started. The move_grade_levels function will run every year on August 7th.")
-    move_grade_levels_for_all_schools("2023-2024", "2024-2025")
 
 
     # Run the Flask app (if needed)
     # app.run(debug=True)
 
-    try:
-        # Keep the script running
-        while True:
-            time.sleep(2)
-    except (KeyboardInterrupt, SystemExit):
-        #scheduler.shutdown()
+    # try:
+    #     # Keep the script running
+    #     while True:
+    #         time.sleep(2)
+    # except (KeyboardInterrupt, SystemExit):
+    #     scheduler.shutdown()
