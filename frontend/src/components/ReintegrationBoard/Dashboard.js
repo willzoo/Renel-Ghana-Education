5f0a9974b8effb72d1c26cdef9888c1a4c0f1da3
1uisf <ldferrer2004@gmail.com>
2024-06-24 16:30:25 +0000

    deleted schoolboard added reintegrationboard

---
+++ frontend/src/components/ReintegrationBoard/Dashboard.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import AddStudentButton from './AddStudentButton/AddStudentButton';
+import Indicators from './Indicators/Indicators'
+import Students from './Students/Students';
+
+import { faPlus, faArrowRight, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+
+
+import './Dashboard.css'
+import Logo from './Logo/Logo';
+
+function Dashboard(props) {
+    let buttons = {
+        newStudent: { id: "new", text: "Add New Student", icon: faPlus },
+        returningStudent: { id: "returning", text: "Add Returning Student", icon: faArrowDown },
+        transferStudent: { id: "transfer", text: "Add Transfer Student", icon: faArrowRight },
+    }
+
+    return (<section>
+        <div className="main">
+            <div className="header-info">
+                {/* <div>EduTracker Logo will eventually go here :)</div> */}
+                <Logo />
+                <Indicators />
+            </div>
+            <div style={{ height: '20px' }}></div>
+
+            {/* add student buttons */}
+            <div className="add-student-section">
+                <AddStudentButton info={buttons.newStudent} /> {/* Add New Student button */}
+                <div style={{ display: 'inline-block', width: '20px' }}></div> {/* Gap between buttons */}
+                <AddStudentButton info={buttons.returningStudent} /> {/* Add Returning Student button */}
+                <div style={{ display: 'inline-block', width: '20px' }}></div> {/* Gap between buttons */}
+                <AddStudentButton info={buttons.transferStudent} /> {/* Add Transfer Student button */}
+            </div>
+
+
+            <Students />
+
+        </div>
+    </section>
+    );
+}
+
+export default Dashboard;
+
+
