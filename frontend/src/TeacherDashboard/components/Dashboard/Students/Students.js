6801382c59a45793cb16e449fbc55a16365ad628
PatrickKallenbach <pkallenbach1@ufl.edu>
2024-06-24 12:53:05 -0400

    restructured file system

---
+++ frontend/src/TeacherDashboard/components/Dashboard/Students/Students.js
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import './Students.css'
+import StudentItem from "./StudentItem/StudentItem";
+
+import TeacherContext from "../../../../TeacherContext";
+
+function Students() {
+    const { classInfo, setClassInfo } = useContext(TeacherContext).classInfo;
+    const { selectedClass, setSelectedClass } = useContext(TeacherContext).selectedClass;
+    const {selectedStudent, setSelectedStudent} = useContext(TeacherContext).selectedStudent;
+
+    const deselectAll = () => {
+        if (!selectedStudent) return;
+    
+        const studentListElements = Array.from(document.getElementsByClassName('student-list-item'));
+        if (!studentListElements) return;
+    
+        studentListElements.forEach((element) => {
+          element.classList.remove('selected');
+        });
+        
+        setSelectedStudent(null);
+    }
+
+    let studentsList = [];
+    if (selectedClass) {
+        for (let i = 0; i < selectedClass.students.length; i++) {
+            studentsList.push((<StudentItem id={i} data={selectedClass.students[i]} />));
+        }
+    }
+
+    return (
+        <div className="student-section">
+            <div className="students-list-container" onClick={deselectAll}>
+                <h1 className="title">{selectedClass ? selectedClass.class_name : "Class"}</h1>
+                <p className="body">Students</p>
+
+                <div id="students-root" className="students-list">
+                    <ul className="nobullet">
+                        {// if selectedClass is null, display error
+                            !selectedClass ? (<li><div className="list-issue">Please select a class to see associated students.</div></li>) :
+                                studentsList.length == 0 ? // if there are no students in the student list
+                                    (<li><div className="list-issue">There are no students that have been added to this class.</div></li>) :
+                                    (studentsList) // display class list if students
+                        }
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Students;
