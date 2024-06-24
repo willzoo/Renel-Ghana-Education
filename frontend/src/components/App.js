0e8231c9d34dcd2c7e82c3369a2fcb9eaf80b37d
willzoo <zhujwill@gmail.com>
2024-06-24 13:02:48 +0000

    Teacher Login Page Working

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,13 +3,15 @@
 
 import { Routes, Route } from 'react-router-dom'
 import TeacherDashboard from './TeacherDashboard';
+import TeacherLogin from './TeacherLogin/TeacherLogin';
 
 function App() {
 
   return (
-    // Teacher context allows for classInfo and selectedClass to be passed throughout the application
+    // Routing between all different pages of the app
     <>
        <Routes>
+          <Route path="/TeacherLogin/" element= {<TeacherLogin />} />
           <Route path="/" element= {<TeacherDashboard />} />
        </Routes>
     </>
